c57f5c333ce63f97e5b1aa6a10bf1e75987e7976
Mohit Singh <golchigeek@gmail.com>
2025-06-09 00:20:33 +0530

    Bug Fix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import {connectDB} from './config/db.config'
+import router from './routes'
 
 const app = express();
 app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+// Register API routes
+app.use('/api', router);
 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     connectDB();
